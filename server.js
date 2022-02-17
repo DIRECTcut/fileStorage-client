17af1ccbcff8bcefd0827b8676ac20cbd0b56c16
Sergey Afonchenko <Sergei.Afonchenko@emergn.com>
2022-02-17 22:23:57 +0300

    Fix heroku config

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
 const app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'public')))
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
