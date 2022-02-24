04415aad8cdc647049bc43b5b7a0ef3b46dbb2cc
Sergey Afonchenko <Sergei.Afonchenko@emergn.com>
2022-02-24 22:18:18 +0300

    Add cors to express server

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+'use strict'
+
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const cors = require('cors')
+
 const app = express()
+const corsOptions = {
+    origin: process.env.VUE_APP_BACKEND_URL,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 app.use(serveStatic(path.join(__dirname, 'public')))
 app.get('*', (req, res) => {
     res.redirect('/')
